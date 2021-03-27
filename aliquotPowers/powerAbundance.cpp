cae83fe9ccc149f9facbf77ff4113f8968983f8b
Alexander Jones <happy5214@gmail.com>
2021-03-27 13:22:27 -0500

    Add GMP programs for aliquot power abundance calculations
    See https://www.mersenneforum.org/showthread.php?t=23612 for more details.

---
+++ aliquotPowers/powerAbundance.cpp
@@ -0,0 +1,157 @@
+/* Aliquot integer power abundance calculator.
+ *
+ * This program scans an exponent range for a given base for an aliquot sum
+ * which can be determined to be abundant based on its factors up to 10^4. It
+ * will output any such exponents, along with the factors that establish
+ * abundance, to a log file.
+ *
+ * (C) Alexander Jones, 2021. My code is under the MIT License, which is
+ * included in this repository.
+ *
+ * Adapted from aliqueit by Mikael Klasson, Christian Beer, et al., which can
+ * be found at https://github.com/ChristianBeer/aliqueit. My understanding of
+ * its rather unique license agreement is that it is public domain with a
+ * disclaimer of warranty.
+ */
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include <gmpxx.h>
+
+using namespace std;
+
+typedef vector<pair<mpz_class, int> > FactorVector;
+
+vector<unsigned int> trial_primes; //precalced primes for trial factoring
+
+void factor(mpz_class n, FactorVector & factors);
+
+void precalc_trial_primes() {
+    cout << "Precalcing primes for trial factoring..." << endl;
+    mpz_class p(2);
+    while (p < 10000) {
+        trial_primes.push_back(p.get_ui());
+        mpz_nextprime(p.get_mpz_t(), p.get_mpz_t());
+    }
+}
+
+//prints a log msg and adds <factor> to <factors>
+
+void found_factor(mpz_class & factor, FactorVector & factors) {
+    factors.push_back(make_pair(factor, 1));
+}
+
+//sorts factors and merges any <p,x>,<p,y> into <p,x+y>
+
+void merge_factors(FactorVector & factors) {
+    sort(factors.begin(), factors.end());
+    for (FactorVector::size_type j = 1; j < factors.size();) {
+        if (factors[j].first == factors[j - 1].first) {
+            factors[j - 1].second += factors[j].second;
+            factors.erase(factors.begin() + j);
+        } else {
+            ++j;
+        }
+    }
+}
+
+//factors <n> and returns its prime components (with exponents) in <factors>.
+//vector<pair<p_i,x_i> >, n = product(p_i^x_i)
+
+void factor(mpz_class n, FactorVector & factors) {
+    factors.clear();
+
+    for (FactorVector::size_type j = 0; j < trial_primes.size(); ++j) {
+        while (mpz_divisible_ui_p(n.get_mpz_t(), trial_primes[j])) {
+            mpz_class tp(trial_primes[j]);
+            found_factor(tp, factors);
+            mpz_divexact_ui(n.get_mpz_t(), n.get_mpz_t(), trial_primes[j]);
+        }
+    }
+
+    merge_factors(factors); //sorts factors and merges any <p,x>,<p,y> into <p,x+y>
+}
+
+//calculates <n>=product(<factors>) and <s>=sigma(<n>)
+
+void sigma(FactorVector & factors, mpz_class & s, mpz_class & n) {
+    mpz_class t, tmp, tmp2;
+    n = 1;
+    s = 1;
+    for (FactorVector::size_type j = 0; j < factors.size(); ++j) {
+        t = 1;
+        mpz_pow_ui(tmp.get_mpz_t(), factors[j].first.get_mpz_t(), factors[j].second + 1);
+        tmp -= 1;
+        tmp2 = factors[j].first - 1;
+        mpz_divexact(t.get_mpz_t(), tmp.get_mpz_t(), tmp2.get_mpz_t());
+
+        s *= t;
+
+        mpz_pow_ui(tmp.get_mpz_t(), factors[j].first.get_mpz_t(), factors[j].second);
+        n *= tmp;
+    }
+}
+
+void print_help() {
+    cout << "usage: powerAbundance <base> <minExp> <maxExp> [<skip>]" << endl;
+}
+
+int main(int argc, char ** argv) {
+    if (argc < 4) {
+        print_help();
+        return 1;
+    }
+
+    mpz_class base;
+    base.set_str(argv[1], 10);
+    int min = atoi(argv[2]);
+    int max = atoi(argv[3]);
+    int skip;
+    if (argc >= 5) {
+        skip = atoi(argv[4]);
+    } else {
+        skip = 2;
+    }
+
+    precalc_trial_primes();
+
+    FactorVector factors; //vector<pair<p_i,x_i> >, n = product(p_i^x_i)
+    mpz_class n, s, partial;
+
+    FactorVector::size_type j;
+    for (int i = min; i <= max; i += skip) {
+        // Index 0 -> 1
+        factor(base, factors);
+        for (j = 0; j < factors.size(); ++j) {
+            factors[j].second *= i;
+        }
+        sigma(factors, s, partial); //calculate sigma(n) and partial = product(factors)
+        n = s - partial;
+
+        // Index 1 -> 2
+        factor(n, factors);
+        sigma(factors, s, partial); //calculate sigma(n) and partial = product(factors)
+        n = s - partial;
+
+        // Check for abundance of partial factors.
+        if (n > partial) {
+            ofstream fff("power_abundant_exponents", ios::app);
+            if (!fff.is_open()) {
+                cout << "WARNING: couldn't open output file for writing!" << endl;
+                return 1;
+            }
+            string factorization = "";
+            for (j = 0; j < factors.size(); ++j) {
+                if (j) factorization += " * ";
+                factorization += factors[j].first.get_str() + (factors[j].second > 1 ? ("^" + to_string(factors[j].second)) : "");
+            }
+            fff << base.get_str() << " " << i << " (" << factorization << ")" << endl;
+            fff.close();
+            cout << base.get_str() << "^" << i << " is abundant!" << endl;
+        }
+    }
+    return 0;
+}
---
+++ aliquotPowers/verifyPrimePowerAbundance.cpp
@@ -0,0 +1,126 @@
+/* Aliquot prime power abundance verifier.
+ *
+ * This program verifies the divisibility and abundance of a set of divisors in
+ * relation to a given prime base and exponent pair.
+ *
+ * (C) Alexander Jones, 2021. My code is under the MIT License, which is
+ * included in this repository.
+ *
+ * Adapted from aliqueit by Mikael Klasson, Christian Beer, et al., which can
+ * be found at https://github.com/ChristianBeer/aliqueit. My understanding of
+ * its rather unique license agreement is that it is public domain with a
+ * disclaimer of warranty.
+ */
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include <gmpxx.h>
+
+using namespace std;
+
+typedef vector<pair<mpz_class, int> > FactorVector;
+
+// adds <factor> to <factors>
+
+void found_factor(mpz_class & factor, FactorVector & factors, int exponent = 1) {
+    factors.push_back(make_pair(factor, exponent));
+}
+
+void load_factors(FactorVector & factors) {
+    ifstream factorFile("partial_factors");
+    if (!factorFile.is_open()) {
+        cout << "WARNING: couldn't open input file for reading!" << endl;
+        exit(1);
+    }
+    string line;
+    while (getline(factorFile, line)) {
+        mpz_class factor;
+        int caretIndex = line.find("^");
+        int power;
+        if (caretIndex == string::npos) {
+            factor.set_str(line, 10);
+            power = 1;
+        } else {
+            factor.set_str(line.substr(0, caretIndex), 10);
+            power = stoi(line.substr(caretIndex + 1));
+        }
+        found_factor(factor, factors, power);
+    }
+    factorFile.close();
+}
+
+//calculates <n>=product(<factors>) and <s>=sigma(<n>)
+
+void sigma(FactorVector & factors, mpz_class & s, mpz_class & n) {
+    mpz_class t, tmp, tmp2;
+    n = 1;
+    s = 1;
+    for (FactorVector::size_type j = 0; j < factors.size(); ++j) {
+        t = 1;
+        mpz_pow_ui(tmp.get_mpz_t(), factors[j].first.get_mpz_t(), factors[j].second + 1);
+        tmp -= 1;
+        tmp2 = factors[j].first - 1;
+        mpz_divexact(t.get_mpz_t(), tmp.get_mpz_t(), tmp2.get_mpz_t());
+
+        s *= t;
+
+        mpz_pow_ui(tmp.get_mpz_t(), factors[j].first.get_mpz_t(), factors[j].second);
+        n *= tmp;
+    }
+}
+
+void print_help() {
+    cout << "usage: verifyPrimePowerAbundance <base> <exponent>" << endl
+         << "Place partial factorization (one factor per line) in file 'partial_factors'" << endl;
+}
+
+int main(int argc, char ** argv) {
+    if (argc < 3) {
+        print_help();
+        return 1;
+    }
+
+    mpz_class base;
+    base.set_str(argv[1], 10);
+    long exponent = atol(argv[2]);
+
+    FactorVector factors; //vector<pair<p_i,x_i> >, n = product(p_i^x_i)
+    load_factors(factors);
+
+    // Validate abundance
+    mpz_class n, s, partial;
+    sigma(factors, s, partial); //calculate sigma(n) and partial = product(factors)
+    n = s - partial;
+    if (n > partial) {
+        cout << "Index 1 of " << base.get_str() << "^" << exponent << " is abundant!" << endl;
+    } else {
+        cout << "Index 1 of " << base.get_str() << "^" << exponent << " is not abundant." << endl;
+    }
+
+    // Validate divisibility
+    mpz_class base_modulo = base - 1;
+    mpz_class modulo;
+    mpz_class result;
+    bool allFactorsDivide = true;
+    for (FactorVector::size_type i = 0; i < factors.size(); ++i) {
+        mpz_class factor;
+        mpz_pow_ui(factor.get_mpz_t(), factors[i].first.get_mpz_t(), factors[i].second);
+        modulo = base_modulo * factor;
+        mpz_powm_ui(result.get_mpz_t(), base.get_mpz_t(), exponent, modulo.get_mpz_t());
+        if (mpz_cmp_ui(result.get_mpz_t(), 1)) {
+            if (factors[i].second > 1) {
+                cout << "Does not divide: " << factors[i].first.get_str() << "^" << factors[i].second << endl;
+            } else {
+                cout << "Does not divide: " << factor.get_str() << endl;
+            }
+            allFactorsDivide = false;
+        }
+    }
+    if (allFactorsDivide) {
+        cout << "All factors divide!" << endl;
+    }
+    return 0;
+}
