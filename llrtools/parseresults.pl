8d77ea449826c1e32e0a39cb149ee545c8e86680
Alexander Jones <happy5214@gmail.com>
2022-10-23 22:40:05 -0500

    Standardize result parsing script

--- llrtools/parseresults.pl
+++ llrtools/parseresults.pl
@@ -1,36 +1,42 @@
 #!/usr/bin/perl
 
-my $decimalregex = qr/[+-]?(\d+\.\d+|\d+\.|\.\d+)/;
+use 5.006;
+use strict;
+use warnings;
+
+my $decimal_regex = qr/[+-]?(\d+\.\d+|\d+\.|\.\d+)/;
 
 my $results_filename = shift // 'lresults.txt';
+my $maxlen_filename = shift // 'maxlen.txt';
 
 my @fftlens = ();
-my $maxlen_filename = 'maxlen.txt';
-open(my $maxlen_file, '<', $maxlen_filename);
+
+open(my $maxlen_file, '<', $maxlen_filename) or die "Could not open max length file: $!";
 while (<$maxlen_file>) {
 	chomp;
 	my @pair = split;
 	push @fftlens, 0 + $pair[0];
 }
-close($maxlen_file);
+close($maxlen_file) or warn "Could not close max length file: $!";
 
 my %times = ();
 
 my $index = 0;
-open(my $results_file, '<', $results_filename);
+
+open(my $results_file, '<', $results_filename) or die "Could not open LLR results file: $!";
 while (<$results_file>) {
 	chomp;
 	my $result = $_;
 	my ($n) = ($result =~ /\d+\*2\^(\d+)-1 is not prime\./);
-	my ($ms) = ($result =~ /Time : $decimalregex ms\./);
+	my ($ms) = ($result =~ /Time : $decimal_regex ms\./);
 	unless ($ms) {
-		my ($seconds) = ($result =~ /Time : $decimalregex sec\./);
+		my ($seconds) = ($result =~ /Time : $decimal_regex sec\./);
 		$ms = $seconds * 1000;
 	}
 	$times{$fftlens[$index]} = $ms / $n;
 	$index++;
 }
-close($results_file);
+close($results_file) or warn "Could not close LLR results file: $!";
 
 printf "%8s%8s\n", 'FFT len', 'Time';
 for my $fftlen (@fftlens) {
